f712d534e674f2b33a902ac6a3c33407c037a017
Adriel Djuned <fakingguy77@gmail.com>
2026-05-22 23:14:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    console.log('🐣 Welcome to Adriel\'s profile page!');
+    console.log('Welcome to Adriel\'s profile page!');
     
     const photo = document.querySelector('.profile-photo');
     if (photo) {
         photo.addEventListener('click', () => {
-            alert('👋 Thanks for visiting!');
+            alert('Thanks for visiting!');
         });
     }
     
